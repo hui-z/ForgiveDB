29ccbca4d32be398447a8e0492ab005fcf8c5507
Lirian Su <liriansu@gmail.com>
2017-07-04 21:58:32 +0800

    feat: add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+
+from setuptools import setup
+
+setup(
+    name='forgive',
+    version='0.0.1',
+    description='A minimal json file database.',
+    author='Lirian Su',
+    author_email='liriansu@gmail.com',
+    url='https://github.com/hui-z/ForgiveDB',
+    license='WTFPL',
+    packages=['forgive'],
+)
