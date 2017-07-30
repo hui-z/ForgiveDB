18c62b90eb8f83c58491e77ff7fd2127d5589611
Lirian Su <liriansu@gmail.com>
2017-07-31 00:12:16 +0800

    update: improve code smell
    * our code is fragrant!

--- forgive/__init__.py
+++ forgive/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
 
-from forgive.db import ForgiveDB
+from forgive.db import ForgiveDB  # NOQA
+
+__version__ = '1.0.0'
 
 __all__ = [
     '__version__'
     'ForgiveDB',
 ]
-
-__version__ = '1.0.0'
